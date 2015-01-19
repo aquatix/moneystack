aad44c837dfe521148e649f57a50667f7ee1bb75
Michiel Scholten <michiel@aquariusoft.org>
2015-01-19 21:37:55 +0100

    Core models

--- moneystack/core/models.py
+++ moneystack/core/models.py
@@ -1,3 +1,62 @@
 from django.db import models
+from datetime import datetime
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class BaseModel(models.Model):
+    """
+    Base model with common properties.
+    """
+    date_created = models.DateTimeField(auto_now_add=True, null=True)
+    date_modified = models.DateTimeField(auto_now=True, null=True)
+
+    class Meta:
+        abstract = True
+        ordering = ('-date_created', )
+
+
+class Project(BaseModel):
+    """
+    Account
+    """
+    title = models.CharField(max_length=255, blank=False)
+    #users
+
+
+class Account(BaseModel):
+    """
+    Banking account
+    """
+    project = models.ForeignKey(Project)
+    title = models.CharField(max_length=255, blank=False)
+    accountcode = models.TextField(max_length=40, blank=False, help_text='Account number, like an IBAN code: NLkk bbbb cccc cccc cc')
+
+    @property
+    def total(self):
+        amount = 0
+        for transaction in self.transactions:
+            amount = amount + transaction.real_amount
+        return amount
+
+
+class Transaction(BaseModel):
+    """
+    A single transaction in an account
+    """
+    date = models.DateTimeField()
+    description = models.TextField()
+    account = models.ForeignKey(Account, related_name='transactions')
+    otheraccount = models.CharField(max_length=40, blank=True)
+    code = models.CharField(max_lengt=255, blank=True)
+    # withdrawal or deposit, True or False
+    withdrawal = models.BooleanField(default=True)
+    amount = models.DecimalField()
+    mutation_kind = models.CharField(max_length=255, blank=True)
+    notes = models.TextField(blank=True)
+
+    @property
+    def real_amount(self):
+        if withdrawal:
+            return -1 * amount
+        else:
+            return amount
